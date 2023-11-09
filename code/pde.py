995a76a00f0b02a7c4f24bda4f76555e09ca7ea3
jwd102 <jerry.worthy@duke.edu>
2023-11-09 14:33:53 -0500

    added more test images

--- code/pde.py
+++ code/pde.py
@@ -2,16 +2,12 @@ import numpy as np
 import cv2 as cv
 
 
-
-
-if __name__ == "__main__":
-    image = cv.imread("../data/abbeyroad.jpg")
-    # Defne the damaged region (rectangle)
-    damage_rect = (120, 160, 10, 10)  # (x, y, width, height)
+def test(damage_rect, image_name):
+    image = cv.imread(image_name)
     # Create a mask to represent the damaged area
     mask = np.zeros(image.shape[:2], dtype="uint8")
     cv.rectangle(mask, (damage_rect[0], damage_rect[1]),
-                  (damage_rect[0] + damage_rect[2], damage_rect[1] + damage_rect[3]), 255, -1)
+                 (damage_rect[0] + damage_rect[2], damage_rect[1] + damage_rect[3]), 255, -1)
     # Damage the image by turning the patch black
     image[damage_rect[1]:damage_rect[1] + damage_rect[3], damage_rect[0]:damage_rect[0] + damage_rect[2]] = 0
     # Use the inpainting function to restore the damaged area
@@ -23,3 +19,8 @@ if __name__ == "__main__":
     # Wait for a key press and then close the windows
     cv.waitKey(0)
     cv.destroyAllWindows()
+
+if __name__ == "__main__":
+    # test((120, 160, 10, 10), "../data/abbeyroad.jpg")
+    # test((180, 160, 50, 50), "../data/cat_drawing.jpg")
+    test((500, 250, 100, 100), "../data/rocks.jpg")

8c40973bc54ce016b54b841965639915fa54310e
jwd102 <jerry.worthy@duke.edu>
2023-12-03 23:42:31 -0500

    added border of initial patches to input patch

--- src/main.py
+++ src/main.py
@@ -4,14 +4,15 @@ from src.restore import restore
 import numpy as np
 
 
-def test(damage_rect, texture_rectangle, image_name, patch_width=10.0, alpha=0.43, iterations=1):
+def test(damage_rect, image_name, patch_width=10.0, alpha=0.43, iterations=1, texture_radius=40, inpainting_radius=10, algorithm=cv.INPAINT_NS):
     damaged_image, mask = load_damaged_image(damage_rect, image_name)
-    cv.imshow("Mask", mask)
-    restored_image = restore(damaged_image, mask, damage_rect, texture_rectangle, patch_width, alpha, iterations)
+    restored_image = restore(damaged_image, mask, damage_rect, patch_width, alpha, iterations, texture_radius, inpainting_radius, algorithm)
     cv.imshow("Damaged Image", damaged_image)
     cv.imshow("Restored Image", restored_image)
     cv.waitKey(0)
 
 
 if __name__ == '__main__':
-    test((200, 280, 50, 50), (100, 400, 100, 200), "../data/Granite-Rock.jpg", patch_width=8.0)
+
+    # test((300, 250, 100, 100), "../data/Granite-Rock.jpg")
+    test((300, 250, 100, 100), "../data/Granite-Rock.jpg")
--- src/restore.py
+++ src/restore.py
@@ -4,57 +4,62 @@ import matlab.engine
 import numpy as np
 
 
-def pde_inpaint(image, mask, radius=3):
-    return cv.inpaint(image, mask, inpaintRadius=radius, flags=cv.INPAINT_NS)
+def restore(damaged_image, mask, mask_rect, patch_width=8.0, alpha=0.43, iterations=1.0, texture_radius=20, inpaint_radius=3, inpaint_algorithm=cv.INPAINT_NS):
+    x, y, width, height = mask_rect
+    restored_image = pde_inpaint(damaged_image, cv.cvtColor(mask, cv.COLOR_BGR2GRAY), inpaint_radius, inpaint_algorithm)
+    initial_patch = create_input_patch(mask, restored_image, int(patch_width))
+    source_texture = create_texture_patch(mask, restored_image, texture_radius)
+    texture_patch = texture_transfer(source_texture, initial_patch, patch_width, alpha, iterations)
+    extract_middle(texture_patch, restored_image[y:y+height, x:x+width])
+    return restored_image
 
 
-def texture_transfer(input_texture, target_image, w=10.0, al=0.43, i=1):
+def pde_inpaint(image, mask, radius=3, algorithm=cv.INPAINT_NS):
+    return cv.inpaint(image, mask, inpaintRadius=radius, flags=algorithm)
+
+
+def texture_transfer(input_texture, target_image, width, alpha, iterations):
     eng = matlab.engine.start_matlab()
     path = "./transfer_script"
     eng.addpath(path, nargout=0)
     input_texture = np.ascontiguousarray(input_texture)
     target_image = np.ascontiguousarray(target_image)
-    result = eng.transfer(input_texture.astype(float) / 255.0, target_image.astype(float) / 255.0, w, al, i, nargout=1)
+    result = eng.transfer(input_texture.astype(float) / 255.0, target_image.astype(float) / 255.0, width, alpha, iterations, nargout=1)
     eng.quit()
     return np.array(result) * 255
 
 
-def restore(damaged_image, mask, mask_rect, source_rect, patch_width=10.0, alpha=0.43, iterations=1):
-    x, y, width, height = mask_rect
-    restored_image = pde_inpaint(damaged_image, cv.cvtColor(mask, cv.COLOR_BGR2GRAY))
-    cv.imshow("Initial Restore", restored_image)
-
-    initial_patch = extract_rectangle(restored_image, mask_rect)
-    source_texture = create_texture_patch(mask, restored_image)
-    # source_texture = extract_rectangle(restored_image, source_rect)
-    cv.imshow("Source Texture", source_texture)
-    cv.waitKey(0)
-    texture_patch = texture_transfer(source_texture, initial_patch, patch_width, alpha, iterations)
-    restored_image[y:y+height, x:x+width] = texture_patch
-    return restored_image
+def create_texture_patch(mask, image, radius):
+    new_mask, mask_rect = square_border(mask, radius)
+    new_image = cv.bitwise_and(new_mask, image)
+    return extract_rectangle(new_image, mask_rect)
 
 
-def square_border(mask, scale=1.5):
+def create_input_patch(mask, image, radius):
+    new_mask, mask_rect = square_border(mask, radius)
+    return extract_rectangle(image, mask_rect)
+
+
+def square_border(mask, radius):
     nonzero_pixels = np.column_stack(np.where(mask > 0))
     min_coords = np.min(nonzero_pixels, axis=0)
     max_coords = np.max(nonzero_pixels, axis=0)
     min_x, min_y = min_coords[1], min_coords[0]
     max_x, max_y = max_coords[1], max_coords[0]
-    center_x = (min_x + max_x) // 2
-    center_y = (min_y + max_y) // 2
-    new_width = int((max_x - min_x) * scale)
-    new_height = int((max_y - min_y) * scale)
-    new_min_x = center_x - new_width // 2
-    new_min_y = center_y - new_height // 2
-    new_max_x = center_x + new_width // 2
-    new_max_y = center_y + new_height // 2
+    new_min_x = max(0, min_x - radius)
+    new_min_y = max(0, min_y - radius)
+    new_max_x = min(mask.shape[1] - 1, max_x + radius)
+    new_max_y = min(mask.shape[0] - 1, max_y + radius)
     final = np.zeros_like(mask)
     cv.rectangle(final, (new_min_x, new_min_y), (new_max_x, new_max_y), (255, 255, 255), thickness=cv.FILLED)
     cv.rectangle(final, (min_x, min_y), (max_x, max_y), (0, 0, 0), thickness=cv.FILLED)
     return final, (new_min_x, new_min_y, new_max_x - new_min_x, new_max_y - new_min_y)
 
 
-def create_texture_patch(mask, image, scale=2.0):
-    new_mask, mask_rect = square_border(mask, scale)
-    new_image = cv.bitwise_and(new_mask, image)
-    return extract_rectangle(new_image, mask_rect)
+def extract_middle(larger_matrix, smaller_matrix):
+    larger_shape = np.array(larger_matrix.shape)
+    smaller_shape = np.array(smaller_matrix.shape)
+    start_idx = (larger_shape - smaller_shape) // 2
+    end_idx = start_idx + smaller_shape
+    middle_region = larger_matrix[start_idx[0]:end_idx[0], start_idx[1]:end_idx[1]]
+    smaller_matrix[:, :] = middle_region
